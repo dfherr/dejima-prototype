e38b2cfddcc71af35fe7f89a25f76555c8af4c35
Dennis Herr <dennis.herr@experteer.com>
2019-07-15 15:55:31 -0700

    persist peer groups to database

--- peer/app/libs/dejima_utils.rb
+++ peer/app/libs/dejima_utils.rb
@@ -1,14 +1,10 @@
 module DejimaUtils
 
-    def self.peer_groups
-        PeerGroups.instance.groups
-    end
-
     # called by config/initializers/dejima_create_peer_groups.rb on startup
     def self.create_peer_groups(*models)
         # when creating we begin with all locally known peer groups
-        self.check_local_peer_groups(models)
-        Rails.logger.info "Peers groups: #{peer_groups}"
+        PeerGroups.update self.check_local_peer_groups(models)
+        Rails.logger.info "Peers groups: #{PeerGroups.get}"
         self.detect_peer_groups
     end
 
@@ -23,7 +19,7 @@ module DejimaUtils
         found_groups = Set.new
         remote_peer_groups.each do |remote_peer_group|
             binding.pry
-            peer_groups.each_value do |peer_group|
+            PeerGroups.get.each_value do |peer_group|
                 if peer_group.dejima_tables.intersect? remote_peer_group.dejima_tables
                     found_groups << peer_group
                 end
@@ -40,7 +36,7 @@ module DejimaUtils
             peers = {}
 
             # map peer addresses to peer_groups connected to this peer
-            peer_groups.each_value do |peer_group|
+            PeerGroups.get.each_value do |peer_group|
                 peer_group.peers.each do |peer_address|
                     peers[peer_address] ||= Set.new
                     peers[peer_address] << peer_group
@@ -56,68 +52,12 @@ module DejimaUtils
                 response = DejimaProxy.send_peer_group_request(peer_address, peers[peer_address].to_a)
                 next if response == "connection_error"
                 response.each do |peer_group|
-                    add_new_peer_group peer_group
+                    PeerGroups.add_new_peer_group peer_group
                 end
             end
         end
 
         #self.broadcast_peer_groups
-    end
-
-    # This function adds new_group to the locally known peer_groups
-    #
-    # We need to detect all intersections between our known groups in peer_groups
-    # and the new group that needs to be added to our peer groups
-    #
-    # In our existing peer_groups no peer_group shares any attributes with
-    # another peer group, i.e. each attribute is exactly assigned to one peer group.
-    #
-    # For every intersection between dejima_tables of our peer groups and the new groups
-    # we need to create or update the peer_group of the union of the dejima_tables with
-    # the intersection of the attributes
-    # 
-    # This might create new, larger peer groups and make existing groups obsolete.
-    #
-    # If new_group is disjoint to all local peer groups, it means this group has no
-    # shared attributes and thus no relevance to this peer. It shouldn't have reached
-    # this method in the first place.
-    #
-    # This algorithm could be optimized by removing handled attributes from new_group
-    # on each iteration and breaking early once new_group.attributes is empty
-    # 
-    def self.add_new_peer_group(new_group)
-        peer_groups.each do |peer_group|
-            next if peer_group.dejima_tables.disjoint? new_group.dejima_tables
-            shared_attributes = peer_group.attributes.intersection new_group.attributes
-            next if shared_attributes.empty?
-            dejima_table_union = peer_group.dejima_tables.union new_group.dejima_tables
-            if peer_groups[dejima_table_union]
-                peer_groups[dejima_table_union].update_peers(new_group)
-            else
-                # create combined peer group, if it doesn't already exist
-                combined_peer_group = PeerGroup.new(
-                    dejima_tables: dejima_table_union,
-                    attributes: shared_attributes,
-                    peers: (peer_group.peers.union new_group.peers)
-                )
-                peer_groups[dejima_table_union] = combined_peer_group
-            end
-            # remove attributes that moved to the new union group from existing group
-            # unless the union group and existing group are identical
-            if peer_group.dejima_tables != dejima_table_union
-                updated_group = PeerGroup.new(
-                    dejima_tables: peer_group.dejima_tables,
-                    attributes: (peer_group.attributes - shared_attributes), # shared "moved up" to the union group
-                    peers: peer_group.peers 
-                )
-                if updated_group.attributes.empty?
-                    peer_groups.delete(peer_group.dejima_tables) 
-                else
-                    updated_group.update_peers(new_group)
-                    peer_groups[peer_group.dejima_tables] = updated_group
-                end
-            end
-        end
     end
 
     # used to broadcast updated peer_groups to the whole network
@@ -151,6 +91,7 @@ module DejimaUtils
         #   #<Set>[GovernmentUser,phone]: [ShareWithBank],
         #   #<Set>[GovernmentUser,birthdate]: [ShareWithInsurance]
         # }
+        peer_groups = {}
         attribute_to_dejima_tables.each_pair do |attribute, dejima_tables|
             peer_group = peer_groups[dejima_tables] || PeerGroup.new(dejima_tables: dejima_tables)
             peer_group.attributes << attribute[1]
@@ -161,6 +102,7 @@ module DejimaUtils
         end
         # peer group result example
         #[ShareWithInsurance, ShareWithBank]: {attributes: [first_name, last_name, address], peers: [network_address_peer1,network_address_peer2]}
+        peer_groups
     end
 
 end
--- peer/db/schema.rb
+++ peer/db/schema.rb
@@ -10,18 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_133703) do
+ActiveRecord::Schema.define(version: 2019_07_15_202825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "plsh"
 
-  create_table "bank_users", force: :cascade do |t|
+  create_table "government_users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "iban"
-    t.string "address"
     t.string "phone"
+    t.string "address"
+    t.date "birthdate"
+  end
+
+  create_table "peer_groups_stores", force: :cascade do |t|
+    t.jsonb "peer_groups", default: {}
+    t.boolean "singleton", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["singleton"], name: "index_peer_groups_stores_on_singleton", unique: true
   end
 
 end

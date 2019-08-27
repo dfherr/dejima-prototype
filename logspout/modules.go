1e8a3fb5ea2bec3220accf0be4f8663ba35e1d6e
Dennis Herr <dennis.herr@experteer.com>
2019-08-28 01:59:16 +0200

    don't decode json logs and use udp

--- logspout/modules.go
+++ logspout/modules.go
@@ -2,6 +2,7 @@ package main
 
 import (
   _ "github.com/looplab/logspout-logstash"
+  _ "github.com/gliderlabs/logspout/adapters/syslog"
   _ "github.com/gliderlabs/logspout/transports/udp"
   _ "github.com/gliderlabs/logspout/transports/tcp"
 )

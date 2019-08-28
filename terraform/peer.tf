b696fbdae319ead5084105e5dc7d164e58aafd74
Dennis Herr <dennis.herr@experteer.com>
2019-08-28 22:44:34 +0200

    configured 20 peer swarm

--- terraform/peer.tf
+++ terraform/peer.tf
@@ -36,16 +36,126 @@ resource "google_compute_instance_from_template" "dejima_peer_bank" {
   source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
 }
 
-resource "google_compute_instance_from_template" "dejima_peer_gov" {
-  name = "dejima-peer-gov"
+resource "google_compute_instance_from_template" "dejima_peer_bank2" {
+  name = "dejima-peer-bank2"
+  zone               = "${var.peer_zone1}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_bank3" {
+  name = "dejima-peer-bank3"
   zone               = "${var.peer_zone2}"
 
   source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
 }
 
+resource "google_compute_instance_from_template" "dejima_peer_bank4" {
+  name = "dejima-peer-bank4"
+  zone               = "${var.peer_zone2}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_bank5" {
+  name = "dejima-peer-bank5"
+  zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_bank6" {
+  name = "dejima-peer-bank6"
+  zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_bank7" {
+  name = "dejima-peer-bank7"
+  zone               = "${var.peer_zone4}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_bank8" {
+  name = "dejima-peer-bank8"
+  zone               = "${var.peer_zone5}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_gov" {
+  name = "dejima-peer-gov"
+  zone               = "${var.peer_zone1}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_gov2" {
+  name = "dejima-peer-gov2"
+  zone               = "${var.peer_zone2}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_gov3" {
+  name = "dejima-peer-gov3"
+  zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_gov4" {
+  name = "dejima-peer-gov4"
+  zone               = "${var.peer_zone4}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_gov5" {
+  name = "dejima-peer-gov5"
+  zone               = "${var.peer_zone5}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
 resource "google_compute_instance_from_template" "dejima_peer_insurance" {
   name = "dejima-peer-insurance"
+  zone               = "${var.peer_zone2}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+
+resource "google_compute_instance_from_template" "dejima_peer_insurance2" {
+  name = "dejima-peer-insurance2"
   zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_insurance3" {
+  name = "dejima-peer-insurance3"
+  zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_insurance4" {
+  name = "dejima-peer-insurance4"
+  zone               = "${var.peer_zone3}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_insurance5" {
+  name = "dejima-peer-insurance5"
+  zone               = "${var.peer_zone4}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_insurance6" {
+  name = "dejima-peer-insurance6"
+  zone               = "${var.peer_zone4}"
+
+  source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
+}
+resource "google_compute_instance_from_template" "dejima_peer_insurance7" {
+  name = "dejima-peer-insurance7"
+  zone               = "${var.peer_zone5}"
 
   source_instance_template = "${google_compute_instance_template.dejima_peer_template.self_link}"
 }
